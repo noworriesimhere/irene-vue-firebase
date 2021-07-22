2f7dc3f831b90a2f1de5e6efec18d1736f10675c
Alan Tran <69406020+noworriesimhere@users.noreply.github.com>
2021-07-22 07:56:18 -0400

    added netlify config options

--- src/store/index.js
+++ src/store/index.js
@@ -1,33 +1,10 @@
 import { createStore } from 'vuex'
-
+import home from '../assets/pages/home.json'
 
 
 export default createStore({
   state: {
-    homeCards: [
-      {
-        title: "Flowers and things test",
-        blogPost: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-        photo: "flowers1",
-        main: true,
-      },
-      {
-        title: "Lorem, ipsum.",
-        blogPost: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-        photo: "flowers2",
-      },
-      {
-        title: "third.",
-        blogPost: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-        photo: "flowers3",
-      },
-      {
-        title: "Lorem ipsum dolor sit.",
-        blogPost: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-        photo: "flowers4",
-      },
-    ],
-  
+    homeCards: home,
   },
   mutations: {
   
